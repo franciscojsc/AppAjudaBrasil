c8bf23c1bec0f4a07cb2c1cb44b9b2906977e08a
Francisco Chaves <francisco.jschaves@gmail.com>
2019-10-03 16:26:15 -0300

    Adicionando Modelo de dados

---
+++ app/src/main/java/br/com/franciscochaves/appajudabrasil/Proposicao.kt
@@ -0,0 +1,7 @@
+package br.com.franciscochaves.appajudabrasil
+
+data class Proposicao (var id: Int,
+                       var uri: String,
+                       var siglaTipo: String,
+                       var ano: Int,
+                       var ementa: String)
